eb5d9df972c7fd61cee02306d55a5ecad15e6f9f
Harsh Srivastav <hsrivastav45@gmail.com>
2023-06-14 23:44:34 +0530

    Polymorphism - Using child class in place of parent class

---
+++ src/JavaBasics/Polymorphism/Dinner.java
@@ -0,0 +1,19 @@
+package JavaBasics.Polymorphism;
+
+public class Dinner {
+
+    private void makeDinner(Noodles noodle, String sauce) {
+        noodle.cook();
+
+        System.out.println("Mixing " + noodle.texture + " noodle made from " + noodle.ingredients + " with " + sauce + ".");
+        System.out.println("Dinner is served!");
+    }
+
+    public static void main(String[] args) {
+
+        Dinner dinnerNoodle = new Dinner();
+
+        Noodles spaetzle = new Spaetzle();
+        dinnerNoodle.makeDinner(spaetzle, "soy sauce with chili");
+    }
+}
